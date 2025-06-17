308fad73d873e3abaf3133e90bba18983476ce9c
Cyrus Wong <cywong@vtc.edu.hk>
2025-06-17 15:08:07 +0800

    Refactor RobotAction to use robot_id instead of device_id; add simulator action handling and improve logging. Clean up imports in SpreadsheetLoader for clarity.

--- constant.py
+++ constant.py
@@ -13,3 +13,6 @@ ROBOT_IPS = [
     "http://192.168.137.5:9030",
     "http://192.168.137.6:9030",
 ]
+
+SIMULATOR_BASE_URL = "https://humanoid-robot-simulator-74gfpibg5q-uc.a.run.app"
+SESSION_KEY = "cywong@vtc.edu.hk"
